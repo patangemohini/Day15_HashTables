dfb32af669524d52ca1692793ea10f45776e8852
mohini patange <patangemohini5@gmail.com>
2022-06-06 20:38:37 +0530

    Added : Welcome massage

---
+++ src/com/bl/hash_table/HashTableMain.java
@@ -0,0 +1,7 @@
+package com.bl.hash_table;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+        System.out.println("Data Structure Programs Using generics!!");
+    }
+}
